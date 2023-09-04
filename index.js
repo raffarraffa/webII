ffc5970ddc6ce3602483af12b273746d79b030e0
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-09-04 19:34:28 -0300

    mejoras en rutas
    Me faltaria usar los mildwares

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 import express from 'express';
-import { dirname, join } from 'path';
+// permite leer el directorio de trabajo
+import { dirname } from 'path';
+
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
+console.log(`__filename: ${__filename}`);
 const __dirname = dirname(__filename);
 const tpi = express();
 const port = 8080;
 const __ver = 'v1';
-console.log(__dirname);
-// Define una función para obtener la ruta absoluta al directorio de tu proyecto
-/*
-const getAbsolutePath = (filePath) => {
-  console.log(__dirname);
-  return join(__dirname, filePath);
-};
-*/
-// Configura el middleware para manejar la caché
 const DEV = true;
+// Configura el middleware para manejar la caché
 if (DEV) {
   tpi.use((req, res, next) => {
     res.header("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
@@ -23,8 +18,7 @@ if (DEV) {
     next();
   });
 }
-
-// Configura las rutas
+// rutas
 
 tpi.get(`/${__ver}/`, (req, res) => {
   res.send(`<h1>Trabajo Practico Integrador WEB 2 </h1><h2><em>Deploy realizado ${new Date()} : Método petición ${req.method}  ${req.originalUrl} </em></h2>`);
@@ -34,14 +28,13 @@ tpi.get(`/${__ver}/todas`, (req, res) => {
   res.send(`<h1>Trabajo Practico Integrador WEB 2 </h1><h2><em>Deploy realizado ${new Date()} : Método petición ${req.method}  ${req.originalUrl} </em></h2>`);
 });
 
-// Configura la ruta para servir el archivo HTML
+// ruta  archivo HTML default
 tpi.get('/', (req, res) => {
-  //  const indexPath = getAbsolutePath('views/index.html');
   const indexPath = `${__dirname}/views/index.html`;
   res.sendFile(indexPath);
 });
 
-// Inicia el servidor
+// inicia servidor
 tpi.listen(port, () => {
   console.log(`Servidor Express en ejecución en http://localhost:${port}`);
 });
