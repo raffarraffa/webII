34fd060a8d523ba2c2dc6931b7be2e996c472546
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-09-07 01:48:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ const url = 'https://restcountries.com/v3.1/all';
 const tpi = express();
 const port = 8080;
 const DEV = true;
-tpi.use(compression());
-tpi.use(express.json());
-
 const corsOptions = {
-  origin: '*',
+  origin: '[http://127.0.0.1:5503', '*'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
+  // credentials: true,
   optionsSuccessStatus: 204,
 };
-
+tpi.use(compression());
+tpi.use(express.json());
 tpi.use(cors(corsOptions));
 if (DEV) {
   tpi.use((req, res, next) => {
@@ -38,7 +36,8 @@ tpi.get('/', (req, res) => {
 tpi.post('/respuesta', (req, res) => {
   //res.send(`<h1>Trabajo Practico Integrador WEB 2 </h1><h2><em>respuesta realizada ${new Date()} : Método petición ${req.method}  ${req.originalUrl} </em></h2>`);
   const datos = req.body;
-  res.status(200).send('Solicitud POST recibida con éxito');
+  // res.status(200).send('Solicitud POST recibida con éxito');
+  res.status(200).send();
 });
 tpi.listen(port, () => {
   console.log(`Servidor Express en ejecución en http://localhost:${port}`);
